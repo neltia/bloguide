86cd13239251d77096c3b4de53f3dca99eb94494
Hyunseo <dsz08082@naver.com>
2025-01-04 22:22:59 +0900

    test: Integrate tailwind with fastapi

--- app/main.py
+++ app/main.py
@@ -4,18 +4,38 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
 
+from fastapi_tailwind import tailwind
+from contextlib import asynccontextmanager
+
 from app.core.config import settings
-from app.api.routers import api_router
+from app.controller.api_routers import api_router
+from app.controller.front_routers import front_router
 from app.common.error_handler_custom import generic_exception_handler, http_exception_handler
 
 import time
 import logging
 
+static_files = StaticFiles(directory="app/static")
+
+
+# lifespan:
+#  Because this code is executed before the application starts taking requests,
+#  and right after it finishes handling requests, it covers the whole application lifespan
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # our tailwind get's compiled here!
+    process = tailwind.compile(static_files.directory + "/css/app.css")
+
+    yield  # code after this is called on shutdown
+
+    process.terminate()  # We must terminate the compiler on shutdown to
+    # prevent multiple compilers running in development mode or when watch is enabled.
 
 # app instance
 app = FastAPI(
     title=settings.PROJECT_NAME,
-    version=settings.VERSION
+    version=settings.VERSION,
+    # lifespan=lifespan
 )
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -24,7 +44,7 @@ logger = logging.getLogger(__name__)
 templates = Jinja2Templates(directory="app/templates")
 
 # static
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount("/static", static_files, name="static")
 
 # Custom Error handler
 app.add_exception_handler(HTTPException, http_exception_handler)
@@ -44,6 +64,7 @@ async def log_request_time(request: Request, call_next):
 
 
 # Include API routers
+app.include_router(front_router)
 app.include_router(api_router, prefix=settings.API_PREFIX)
 
 
